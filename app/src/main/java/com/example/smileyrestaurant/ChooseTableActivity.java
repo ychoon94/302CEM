078d9267cc574708f666e73d066ad78c2a20d695
yChoon <36335272+ychoon94@users.noreply.github.com>
2021-05-27 12:35:09 +0800

    finished choosetablescreen xml

--- app/src/main/java/com/example/smileyrestaurant/ChooseTableActivity.java
+++ app/src/main/java/com/example/smileyrestaurant/ChooseTableActivity.java
@@ -3,6 +3,7 @@ package com.example.smileyrestaurant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ChooseTableActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class ChooseTableActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_table);
+    }
+
+    public void menu(View view) {
     }
 }
