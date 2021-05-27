a866f83d83001af7508345e85b83a9fa3241eb5d
jiayee0719 <73810404+jiayee0719@users.noreply.github.com>
2021-05-27 11:47:27 +0800

    Update
    - json file added
    - landscape mode set

--- app/src/main/java/com/example/smileyrestaurant/KitchenActivity.java
+++ app/src/main/java/com/example/smileyrestaurant/KitchenActivity.java
@@ -2,11 +2,12 @@ package com.example.smileyrestaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class Kitchen_Activity extends AppCompatActivity {
+public class KitchenActivity extends AppCompatActivity {
 
     ListView kitchenlist;
 
@@ -14,6 +15,7 @@ public class Kitchen_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kitchen);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         kitchenlist = (ListView) findViewById(R.id.kitchen_list);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.kitchen_list_item);
         kitchenlist.setAdapter(arrayAdapter);
