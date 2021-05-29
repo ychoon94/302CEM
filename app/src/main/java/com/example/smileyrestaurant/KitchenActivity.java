e2f0f286c74e44de832135b8ce3c09eb356416d2
yChoon <36335272+ychoon94@users.noreply.github.com>
2021-05-29 19:34:18 +0800

    kitchen equals error

--- app/src/main/java/com/example/smileyrestaurant/KitchenActivity.java
+++ app/src/main/java/com/example/smileyrestaurant/KitchenActivity.java
@@ -35,24 +35,45 @@ public class KitchenActivity extends AppCompatActivity {
 
         mActivityKitchenBinding = DataBindingUtil.setContentView(this, R.layout.activity_kitchen);
 
-        ordertime1 = getIntent().getStringExtra("table");
-        foodname1 = getIntent().getStringExtra("ordernumber");
-        foodname2 = getIntent().getStringExtra("table");
-        foodname3 = getIntent().getStringExtra("ordernumber");
-        quantity1 = getIntent().getStringExtra("table");
-        quantity2 = getIntent().getStringExtra("ordernumber");
-        quantity3 = getIntent().getStringExtra("table");
-        status1 = getIntent().getStringExtra("ordernumber");
+        ordertime1 = getIntent().getStringExtra("ordertime1");
+        foodname1 = getIntent().getStringExtra("foodname1");
+        foodname2 = getIntent().getStringExtra("foodname2");
+        foodname3 = getIntent().getStringExtra("foodname3");
+        quantity1 = getIntent().getStringExtra("quantity1");
+        quantity2 = getIntent().getStringExtra("quantity2");
+        quantity3 = getIntent().getStringExtra("quantity3");
+        status1 = getIntent().getStringExtra("status1");
 
-        ordertime2 = getIntent().getStringExtra("table");
-        foodname4 = getIntent().getStringExtra("ordernumber");
-        foodname5 = getIntent().getStringExtra("table");
-        foodname6 = getIntent().getStringExtra("ordernumber");
-        quantity4 = getIntent().getStringExtra("table");
-        quantity5 = getIntent().getStringExtra("ordernumber");
-        quantity6 = getIntent().getStringExtra("table");
-        status2 = getIntent().getStringExtra("ordernumber");
+        ordertime2 = getIntent().getStringExtra("ordertime2");
+        foodname4 = getIntent().getStringExtra("foodname4");
+        foodname5 = getIntent().getStringExtra("foodname5");
+        foodname6 = getIntent().getStringExtra("foodname6");
+        quantity4 = getIntent().getStringExtra("quantity4");
+        quantity5 = getIntent().getStringExtra("quantity5");
+        quantity6 = getIntent().getStringExtra("quantity6");
+        status2 = getIntent().getStringExtra("status2");
 
+        Log.d("HELLLLLLLLLLLLLLLLLLLLLOOOOOOOOOOOO", ordertime1);
+        Log.d("HELLLLLLLLLLLLLLLLLLLLLOOOOOOOOOOOO", foodname1);
+
+
+        if (status1.equals("placed")){
+            mActivityKitchenBinding.textViewOrdertime1.setText(ordertime1);
+            mActivityKitchenBinding.textviewFoodname1.setText(foodname1);
+            mActivityKitchenBinding.textViewFoodname2.setText(foodname2);
+            mActivityKitchenBinding.textViewFoodname3.setText(foodname3);
+            mActivityKitchenBinding.textViewQuantity1.setText(quantity1);
+            mActivityKitchenBinding.textViewQuantity2.setText(quantity2);
+            mActivityKitchenBinding.textViewQuantity3.setText(quantity3);
+
+            mActivityKitchenBinding.textViewOrdertime2.setText(ordertime2);
+            mActivityKitchenBinding.textViewFoodname4.setText(foodname4);
+            mActivityKitchenBinding.textViewFoodname5.setText(foodname5);
+            mActivityKitchenBinding.textViewFoodname6.setText(foodname6);
+            mActivityKitchenBinding.textViewQuantity4.setText(quantity4);
+            mActivityKitchenBinding.textViewQuantity5.setText(quantity5);
+            mActivityKitchenBinding.textViewQuantity6.setText(quantity6);
+        }
 
     }
 
