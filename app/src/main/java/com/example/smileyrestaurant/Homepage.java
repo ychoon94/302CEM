b533e4dc218f93f1ce61d55d131fab6e77c89012
Logeswari <58634670+loges5350@users.noreply.github.com>
2021-06-02 23:13:10 +0800

    Homepage Update
    - Link Add Order button to Add Order  screen
    - Link Kitchen button to Kitchen screen

--- app/src/main/java/com/example/smileyrestaurant/Homepage.java
+++ app/src/main/java/com/example/smileyrestaurant/Homepage.java
@@ -1,7 +1,11 @@
 package com.example.smileyrestaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Homepage extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        Button button = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(v -> {
+            Intent intent1 = new Intent(Homepage.this,AddOrder.class);
+            startActivity(intent1);
+        });
+
+        button2.setOnClickListener(v -> {
+            Intent intent2 = new Intent(Homepage.this, Kitchen.class);
+            startActivity(intent2);
+        });
     }
 }
