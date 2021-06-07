ae283414a2d8567cbb50e911978772c05d3089d6
Logeswari <58634670+loges5350@users.noreply.github.com>
2021-06-07 23:03:21 +0800

    Update Add Order & Kitchen Screen

--- app/src/main/java/com/example/smileyrestaurant/AddOrder.java
+++ app/src/main/java/com/example/smileyrestaurant/AddOrder.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AddOrder extends AppCompatActivity {
-
+    //Add Order Screen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
