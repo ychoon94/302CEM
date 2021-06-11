e86aefbcc93cd37c346d091ecdc6ebfadf8210ea
Logeswari <58634670+loges5350@users.noreply.github.com>
2021-06-11 23:24:17 +0800

    Update Homepage

--- app/src/main/java/com/example/smileyrestaurant/AddOrder.java
+++ app/src/main/java/com/example/smileyrestaurant/AddOrder.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AddOrder extends AppCompatActivity {
-
+//AddOrder Screen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
