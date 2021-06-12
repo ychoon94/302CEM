e2a52ac69abab354c64fcc0a9c6a3faacde69fe7
Logeswari <58634670+loges5350@users.noreply.github.com>
2021-06-12 22:20:57 +0800

    Update Homepage

--- app/src/main/java/com/example/smileyrestaurant/Kitchen.java
+++ app/src/main/java/com/example/smileyrestaurant/Kitchen.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Kitchen extends AppCompatActivity {
-//Kitchen Screen
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
