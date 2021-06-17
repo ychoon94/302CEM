938eca545e6377b563cd949d2b9631ebdccfb7ee
yChoon <36335272+ychoon94@users.noreply.github.com>
2021-06-17 15:04:55 +0800

    merge with unit testing hy part

---
+++ app/src/test/java/com/example/smileyrestaurant/LoginActivityTest.java
@@ -0,0 +1,34 @@
+package com.example.smileyrestaurant;
+
+import com.example.smileyrestaurant.model.Staff;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LoginActivityTest {
+
+    List<Staff> staffList = new ArrayList<Staff>();
+
+    @Mock
+    Staff mike = new Staff("chef","p455w0rd", "kitchen");
+    Staff adam = new Staff("waiter","p455w0rd", "welcome");
+
+    @Before
+    public void init(){
+        staffList.add(mike);
+        staffList.add(adam);
+    }
+
+    @Test
+    public void loginAccount() {
+        assertEquals(mike.getRole(), "kitchen");
+        assertEquals(adam.getPassword(), "p455w0rd");
+        assertEquals(mike.getPassword(),"123456");
+    }
+}
