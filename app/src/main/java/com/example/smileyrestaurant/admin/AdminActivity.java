2790a3d6827399ee19f1dcbcfda1d8f5a0b240c5
yChoon <36335272+ychoon94@users.noreply.github.com>
2021-06-18 21:45:37 +0800

    Created admin account screen

--- app/src/main/java/com/example/smileyrestaurant/admin/AdminActivity.java
+++ app/src/main/java/com/example/smileyrestaurant/admin/AdminActivity.java
@@ -2,15 +2,30 @@ package com.example.smileyrestaurant.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.smileyrestaurant.R;
+import com.example.smileyrestaurant.databinding.ActivityAdminBinding;
 
 public class AdminActivity extends AppCompatActivity {
+
+    ActivityAdminBinding mActivityAdminBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+    }
+
+    public void manageStaff(View view) {
+        Intent managestaff = new Intent(AdminActivity.this, AccountActivity.class);
+        startActivity(managestaff);
+    }
+
+    public void manageMenuListing(View view) {
+        Intent manageMenu = new Intent(AdminActivity.this, UpdateMenuActivity.class);
+        startActivity(manageMenu);
     }
 }
