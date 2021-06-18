080a2dd901cca1937c99991fe8497a5ac24636a1
yChoon <36335272+ychoon94@users.noreply.github.com>
2021-06-18 19:39:08 +0800

    creating screen for sprint 3

---
+++ app/src/main/java/com/example/smileyrestaurant/admin/UpdateMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.smileyrestaurant.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_menu);
+    }
+}
