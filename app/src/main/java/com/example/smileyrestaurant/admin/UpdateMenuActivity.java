34055db0414de79d1426b55ae1ac255319ffbb4d
yChoon <36335272+ychoon94@users.noreply.github.com>
2021-06-18 19:41:02 +0800

    creating screen for sprint 3

--- app/src/main/java/com/example/smileyrestaurant/admin/UpdateMenuActivity.java
+++ app/src/main/java/com/example/smileyrestaurant/admin/UpdateMenuActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.smileyrestaurant.R;
+
 public class UpdateMenuActivity extends AppCompatActivity {
 
     @Override
