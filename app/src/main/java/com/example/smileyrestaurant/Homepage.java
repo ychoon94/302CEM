a803a15db2d8570a238bcf8582e213690ac8cf2f
Logeswari <58634670+loges5350@users.noreply.github.com>
2021-06-19 21:38:33 +0800

    Update Homepage

--- app/src/main/java/com/example/smileyrestaurant/Homepage.java
+++ app/src/main/java/com/example/smileyrestaurant/Homepage.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class Homepage extends AppCompatActivity {
-
+//function for add order/kitchen button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
