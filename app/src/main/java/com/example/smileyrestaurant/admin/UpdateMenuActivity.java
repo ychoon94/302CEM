8c61564d3d49878cf66bd3f726c8d29013aa7c11
yChoon <36335272+ychoon94@users.noreply.github.com>
2021-06-20 12:09:02 +0800

    admin menu activity done

--- app/src/main/java/com/example/smileyrestaurant/admin/UpdateMenuActivity.java
+++ app/src/main/java/com/example/smileyrestaurant/admin/UpdateMenuActivity.java
@@ -3,6 +3,7 @@ package com.example.smileyrestaurant.admin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.smileyrestaurant.R;
 
@@ -12,5 +13,8 @@ public class UpdateMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_menu);
+    }
+
+    public void addnewfood(View view) {
     }
 }
