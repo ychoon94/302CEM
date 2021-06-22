0eb9e70a97cd44250e2f2bfc73d77d4b955e2307
yChoon <36335272+ychoon94@users.noreply.github.com>
2021-06-22 13:26:15 +0800

    create add menu item screen

--- app/src/main/java/com/example/smileyrestaurant/admin/AddNewMenuActivity.java
+++ app/src/main/java/com/example/smileyrestaurant/admin/AddNewMenuActivity.java
@@ -1,16 +1,48 @@
 package com.example.smileyrestaurant.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.smileyrestaurant.R;
+import com.example.smileyrestaurant.databinding.ActivityAddNewMenuBinding;
+import com.example.smileyrestaurant.databinding.ActivityUpdateMenuBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddNewMenuActivity extends AppCompatActivity {
+
+    ActivityAddNewMenuBinding mActivityAddNewMenu;
+
+    Spinner spinner;
+
+    List<String> menuvisibility;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_menu);
+
+        mActivityAddNewMenu = DataBindingUtil.setContentView(this, R.layout.activity_add_new_menu);
+
+        spinner = findViewById(R.id.spinner_menuvisibility);
+
+        menuvisibility = new ArrayList<String>();
+        menuvisibility.add("visible");
+        menuvisibility.add("hide");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, menuvisibility);
+
+        dataAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        spinner.setAdapter(dataAdapter);
+    }
+
+    public void savenewmenuitem(View view) {
     }
 }
