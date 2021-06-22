ec9e4bae224ba9cccd2cc6b57ba30acf5079453e
Logeswari <58634670+loges5350@users.noreply.github.com>
2021-06-23 00:04:46 +0800

    Update Add Order & Kitchen

--- app/src/main/java/com/example/smileyrestaurant/Kitchen.java
+++ app/src/main/java/com/example/smileyrestaurant/Kitchen.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class Kitchen extends AppCompatActivity {
 //Test Screen for Kitchen
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
