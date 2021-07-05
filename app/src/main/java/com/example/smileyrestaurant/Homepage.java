1d83b54451c33581232326d5d7f0516a09f7880e
Logeswari <58634670+loges5350@users.noreply.github.com>
2021-07-05 20:41:28 +0800

    Update

--- app/src/main/java/com/example/smileyrestaurant/Homepage.java
+++ app/src/main/java/com/example/smileyrestaurant/Homepage.java
@@ -15,7 +15,6 @@ public class Homepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-
         Button button = findViewById(R.id.button);
         button.setOnClickListener(v -> {
             Intent intent1 = new Intent(Homepage.this,AddOrder.class);
